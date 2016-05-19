b8fb3a9876915e58038832691ff64567083143e4
Bendik Samseth <b.samseth@gmail.com>
2016-05-19 12:17:57 +0200

    Added solution comment to 28

--- 28/28.py
+++ 28/28.py
@@ -10,6 +10,11 @@ Starting with the number 1 and moving to the right in a clockwise direction a 5 
 It can be verified that the sum of the numbers on the diagonals is 101.
 
 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+
+Solution comment:
+The inclueded numbers are increasing by 2 four times, then increasing by
+4 four times, then by 6 four times... This is done (dim-1)/2 times. For dim=5
+above, this is done 2 times.
 """
 
 n = 1
