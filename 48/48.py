fd3f22373158def8120320d1ddba1c8e0a42451f
Bendik Samseth <b.samseth@gmail.com>
2016-05-24 12:57:41 +0200

    Added extra solution to 48

--- 48/48.py
+++ 48/48.py
@@ -9,3 +9,19 @@ Arbirary precision integers for the win.
 """
 
 print sum(i**i for i in xrange(1, 1000+1)) % int(1e10)
+
+
+# in case of non-arbitrary precision, keep only
+# the trailing 10 digits. This means doing the exponentiation
+# manualy, holding the total number of digits at a low number.
+s = 0
+modulo = int(1e10)
+for i in xrange(1, 1001):
+    tmp = i  # tmp will be the trailing digits of the i**i
+    for j in xrange(1, i):
+        tmp *= i
+        if tmp >= modulo / i:
+            tmp %= modulo
+
+    s = (s + tmp) % modulo
+print s
