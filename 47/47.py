7749bfe4acdf5e461ae05bc6d98e1bbbb5e3e462
Bendik Samseth <b.samseth@gmail.com>
2016-05-24 12:23:36 +0200

    Added solution to 47

---
+++ 47/47.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""
+The first two consecutive numbers to have two distinct prime factors are:
+
+14 = 2 × 7
+15 = 3 × 5
+
+The first three consecutive numbers to have three distinct prime factors are:
+
+644 = 2² × 7 × 23
+645 = 3 × 5 × 43
+646 = 2 × 17 × 19.
+
+Find the first four consecutive integers to have four distinct prime factors. What is the first of these numbers?
+"""
+
+from projecteuler.prime import primes_dict
+from itertools import count
+
+consecutive = 4
+
+consec_list = []
+for n in count(2):
+    p = primes_dict(n)
+    if len(p) != consecutive:
+        consec_list = []
+        continue
+    else:
+        consec_list.append(n)
+
+    if len(consec_list) == consecutive:
+        break
+print consec_list
