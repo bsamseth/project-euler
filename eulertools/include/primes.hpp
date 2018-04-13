6e774a096d2f13bac0db154973261e7cb3396058
Bendik Samseth <b.samseth@gmail.com>
2018-04-13 12:36:57 +0200

    Implement Miller-Rabin primality check.

--- eulertools/include/primes.hpp
+++ eulertools/include/primes.hpp
@@ -78,4 +78,46 @@ Integer number_of_divisors(Integer n, Power power = 1) {
     }
     return count;
 }
+
+/**
+ * Miller-Rabin test for primality.
+ * If the return is false, then n is definitively composite.
+ * If the return is true, n is probably prime, with accuracy depending
+ * on the value of k. k = 40 -> prob of false positive ~2^-80.
+ *
+ * See https://en.wikipedia.org/wiki/Miller-Rabin_primality_test
+ */
+template<typename Integer>
+bool is_prime(Integer n, int k = 20) {
+    if ( n == 2 or n == 3 ) return true;
+    if ( n <= 1 or even(n) ) return false;
+
+    static std::default_random_engine generator;
+    std::uniform_int_distribution<Integer> distribution(2, n - 2);
+
+    Integer r = 0, d = n - 1;
+    for (; even(d); ++r, d >>= 1);
+
+    for (int check = 0; check < k; ++check) {
+        Integer a = distribution(generator);
+
+        Integer x = pow_mod(a, d, n);
+        if (x == 1 or x == n - 1)
+            continue;
+
+        for (Integer inner_check = 0; inner_check < r - 1; ++inner_check) {
+            x = pow_mod(x, 2, n);
+            if (x == 1)
+                return false;
+            if (x == n - 1)
+                goto continue_checks;
+        }
+        return false;
+
+        continue_checks:
+        continue;
+    }
+    return true;
+}
+
 }
