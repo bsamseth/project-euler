cfbf03a43eb5b6c87e45fdd4f65a877b9b4e58c3
Bendik Samseth <b.samseth@gmail.com>
2018-04-13 12:31:59 +0200

    Add simple even/odd tests.

--- eulertools/include/tools.hpp
+++ eulertools/include/tools.hpp
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <cassert>
+#include <type_traits>
+
 namespace euler {
+
+/**
+ * Return true if x is odd.
+ */
+template<typename Int>
+inline bool odd(Int x) {
+    static_assert(std::is_integral<Int>::value, "Integral required.");
+    return static_cast<bool>(x & 1);
+}
+/**
+ * Return true if x is even.
+ */
+template<typename Int>
+inline bool even(Int x) {
+    return not odd(x);
+}
 
 /**
  * Convenience squaring template.
